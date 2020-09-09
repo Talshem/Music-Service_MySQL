ee37e85ece2e92cda9e1191325868d25e7c7c437
Talshem <talsh8797@gmail.com>
2020-09-09 16:01:38 +0300

    ALL entry points

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ con.connect(function(err) {
   console.log("Connected!");
 });
 
+///////////////////////////////////////////////////////////////// Get 20
 
   // Get top_songs LIMIT 20
 app.get('/top_songs', (req, res) => {
@@ -30,7 +31,37 @@ app.get('/top_songs', (req, res) => {
     })
   })
 
-    // Get a song LIMIT 20
+  // Get top_songs LIMIT 20
+app.get('/top_artists', (req, res) => {
+  let sql = "SELECT * FROM artists ORDER BY name LIMIT 20";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    res.send(result)
+    })
+  })
+
+  // Get top_songs LIMIT 20
+app.get('/top_albums', (req, res) => {
+  let sql = "SELECT * FROM albums ORDER BY name LIMIT 20";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    res.send(result)
+    })
+  })
+
+  // Get top_songs LIMIT 20
+app.get('/top_playlists', (req, res) => {
+  let sql = "SELECT * FROM playlist ORDER BY name LIMIT 20";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    res.send(result)
+    })
+  })
+
+///////////////////////////////////////////////////////////////// Get 1
+
+
+    // Get a song 
 app.get('/song/:title', (req, res) => {
   let sql = `SELECT * FROM songs WHERE title = ${req.params.title}`;
   con.query(sql, function (err, result) {
@@ -38,6 +69,36 @@ app.get('/song/:title', (req, res) => {
     res.send(result)
     })
   })
+
+      // Get an artist
+app.get('/artist/:name', (req, res) => {
+  let sql = `SELECT * FROM artists WHERE name = ${req.params.name}`;
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    res.send(result)
+    })
+  })
+
+      // Get an album
+app.get('/album/:name', (req, res) => {
+  let sql = `SELECT * FROM albums WHERE name = ${req.params.name}`;
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    res.send(result)
+    })
+  })
+
+      // Get a song LIMIT 20
+app.get('/playlist/:name', (req, res) => {
+  let sql = `SELECT * FROM playlists WHERE name = ${req.params.name}`;
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    res.send(result)
+    })
+  })
+
+
+///////////////////////////////////////////////////////////////// POST
 
 
 // POST song 
@@ -52,6 +113,47 @@ console.log(body)
   });
 });
 
+// POST artist
+app.post('/artist', (req, res) => {
+const body = req.body;
+console.log(body)
+  let sql = `INSERT INTO artists (name, cover_img, upload_at) VALUES 
+  (${body.name}, ${body.cover_img}, ${body.upload_at})`;
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    res.send(result)
+  });
+});
+
+
+// POST album 
+app.post('/album', (req, res) => {
+const body = req.body;
+console.log(body)
+  let sql = `INSERT INTO albums (name, artist, cover_img, created_at, upload_at) VALUES 
+  (${body.name}, ${body.artist}, ${body.cover_img}, ${body.created_at}, ${body.upload_at})`;
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    res.send(result)
+  });
+});
+
+
+// POST playlist
+app.post('/playlist', (req, res) => {
+const body = req.body;
+console.log(body)
+  let sql = `INSERT INTO playlists (name, cover_img, songs, created_at,  upload_at) VALUES 
+  (${body.name}, ${body.cover_img}, ${body.songs}, ${body.created_at}, ${upload_at.length})`;
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    res.send(result)
+  });
+});
+
+
+///////////////////////////////////////////////////////////////// PUT
+
 // PUT song
 app.post('/song', (req, res) => {
 const body = req.body;
@@ -65,9 +167,76 @@ res.send(result)
 });
 });
 
-// delete song
+// PUT artist
+app.post('/artist', (req, res) => {
+const body = req.body;
+console.log(body)
+  let sql = `UPDATE artists SET name = ${body.name}, cover_img = ${body.cover_img}, upload_at = ${body.upload_at} WHERE id = ${body.id}`;
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    res.send(result)
+  });
+});
+
+
+// PUT album 
+app.post('/album', (req, res) => {
+const body = req.body;
+console.log(body)
+  let sql = `UPDATE albums SET name = ${body.name}, artist = ${body.artist}, cover_img = ${body.cover_img},
+  created_at = ${body.created_at}, upload_at = ${body.upload_at} WHERE id = ${body.id}`;
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    res.send(result)
+  });
+});
+
+
+// PUT playlist
+app.post('/playlist', (req, res) => {
+const body = req.body;
+console.log(body)
+  let sql = `UPDATE playlists SET name = ${body.name}, cover_img = ${body.cover_img}, songs = ${body.songs},
+  created_at = ${body.created_at}, upload_at = ${upload_at.length} WHERE id = ${body.id}`;
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    res.send(result)
+  });
+});
+
+
+///////////////////////////////////////////////////////////////// DELETE
+
+// DELETE song
 app.delete('/song/:title', (req, res) => {
-var sql = `DELETE FROM songs WHERE title = ${req.params.title}, id = ${body.id} LIMIT 1 `;
+var sql = `DELETE FROM songs WHERE title = ${req.params.title}, id = ${body.id}`;
+con.query(sql, function (err, result) {
+if (err) throw err;
+res.send(result)
+});
+});
+
+// DELETE album
+app.delete('/album/:name', (req, res) => {
+var sql = `DELETE FROM albums WHERE name = ${req.params.name}, id = ${body.id}`;
+con.query(sql, function (err, result) {
+if (err) throw err;
+res.send(result)
+});
+});
+
+// DELETE artist
+app.delete('/artist/:name', (req, res) => {
+var sql = `DELETE FROM artists WHERE name = ${req.params.name}, id = ${body.id}`;
+con.query(sql, function (err, result) {
+if (err) throw err;
+res.send(result)
+});
+});
+
+// DELETE playlist
+app.delete('/playlist/:name', (req, res) => {
+var sql = `DELETE FROM playlists WHERE name = ${req.params.name}, id = ${body.id}`;
 con.query(sql, function (err, result) {
 if (err) throw err;
 res.send(result)
