750de4b0d497b34a6460d38c96e098e5d558712d
Talshem <talsh8797@gmail.com>
2020-09-09 13:06:44 +0300

    post creating tables

---
+++ index.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+var mysql = require('mysql');
+
+const user = process.env.user
+const password = process.env.password
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: user,
+  password: password,
+  database: "mydb",
+});
+
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+});
