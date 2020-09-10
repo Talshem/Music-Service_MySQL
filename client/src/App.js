eeeff920a15473b26a40093c72e6fa04b65670ba
Talshem <talsh8797@gmail.com>
2020-09-10 20:30:16 +0300

    user configures

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,278 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import '@trendmicro/react-sidenav/dist/react-sidenav.css';
+import SideNav, {
+  Toggle, Nav, NavItem, NavIcon, NavText,
+} from '@trendmicro/react-sidenav';
+import Home from './components/Home.js'
+import Songs from './components/Songs.js'
+import Artists from './components/Artists.js'
+import Playlists from './components/Playlists.js'
+import Albums from './components/Albums.js'
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Button from '@material-ui/core/Button';
+import axios from 'axios'
 
 function App() {
+const [registerOpen, setRegisterOpen] = useState(false)
+const [loginOpen,setLoginOpen] = useState(false)
+const [user, setUser] = useState(undefined);
+
+const [loginEmail, setLoginName] = useState('')
+const [loginPassword, setLoginPassword] = useState('')
+
+const [registerEmail, setRegisterEmail] = useState('')
+const [registerName, setRegisterName] = useState('')
+const [registerPassword, setRegisterPassword] = useState('')
+const [registerRePassword, setRegisterRePassword] = useState('')
+
+  useEffect(() => {
+    const rememberToken = async () => {
+      const { data } = await axios.get(`/token`)
+      setTimeout(() => {
+      setUser(data[0])
+      }, 500);
+    }; rememberToken();
+  }, []);
+
+
+
+  const handleLoginEmail = (event) => {
+  setLoginName(event.target.value)
+  }
+  const handleLoginPassword = (event) => {
+  setLoginPassword(event.target.value)
+  }
+
+    const handleLogin = async () => {
+      const { data } = await axios.put(`/users`, {
+      email: loginEmail,
+      password: loginPassword,
+      });
+
+setUser(data[0])
+setLoginOpen(false)
+    };
+
+const handleLogout = async () => {
+await axios.put(`/logout`);
+setUser(undefined)
+    };
+
+
+  const handleRegisterName = (event) => {
+  setRegisterName(event.target.value)
+  }
+  const handleRegisterEmail = (event) => {
+  setRegisterEmail(event.target.value)
+  }
+  const handleRegisterPassword = (event) => {
+  setRegisterPassword(event.target.value)
+  }
+  const handleRegisterRePassword = (event) => {
+  setRegisterRePassword(event.target.value)
+  }   
+ 
+function validateEmail(mail) 
+{
+ if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(mail))
+  {
+    return (true)
+  }
+    return (false)
+}
+
+  const handleRegister = async () => {
+    try{
+    if(validateEmail(registerEmail)) {
+          if (registerPassword === registerRePassword){
+      const { data } = await axios.post(`/users`, {
+      name: registerName,
+      email: registerEmail,
+      password: registerPassword,
+      });
+setUser(data[0])
+setRegisterOpen(false)
+           } else {
+    document.getElementById('errorMessage').innerHTML='Password fields do not match!';
+           }
+          } else {
+ document.getElementById('errorMessage').innerHTML='Please enter a valid email address';
+           }
+  } catch(response){
+  document.getElementById('errorMessage').innerHTML='The email you tried to register with is already in use';
+  }; 
+  }
+
+const login = 
+  <span>
+<Button variant="text" color="inherit" onClick={() => setLoginOpen(true)}>
+        Login
+      </Button>
+      <Dialog open={loginOpen} onClose={() => setLoginOpen(false)} aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">Login</DialogTitle>
+        <DialogContent>
+          <TextField onChange={handleLoginEmail}
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Email Address"
+            type="email"
+            required
+            fullWidth
+          />
+            <TextField onChange={handleLoginPassword}
+            autoFocus
+            margin="dense"
+            id="password"
+            label="Password"
+            type="password"
+            required
+            fullWidth
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button type="submit" onClick={() => setLoginOpen(false)} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleLogin} color="primary">
+            login
+          </Button>
+        </DialogActions>
+      </Dialog>
+      </span>
+
+
+const register = 
+  <span>
+<Button variant="text" color="inherit" onClick={() => setRegisterOpen(true)}>
+        Register
+      </Button>
+      <Dialog open={registerOpen} onClose={() => setRegisterOpen(false)} aria-labelledby="form-dialog-title">
+        <DialogTitle id="form-dialog-title">Register</DialogTitle>
+        <DialogContent>
+          <TextField onChange={handleRegisterEmail}
+            autoFocus
+            margin="dense"
+            id="email"
+            label="Email Address"
+            type="email"
+            fullWidth
+          />
+          <TextField onChange={handleRegisterName}
+            autoFocus
+            margin="dense"
+            id="name"
+            label="Name"
+            type="name"
+            fullWidth
+          />
+           <TextField onChange={handleRegisterPassword}
+            autoFocus
+            margin="dense"
+            id="password"
+            label="Password"
+            type="password"
+            fullWidth
+          />
+           <TextField onChange={handleRegisterRePassword}
+            autoFocus
+            margin="dense"
+            id="rePassword"
+            label="Confirm password"
+            type="password"
+            fullWidth
+          />
+        </DialogContent>
+        <DialogTitle style={{color:'red'}} id="errorMessage"></DialogTitle>
+        <DialogActions>
+          <Button onClick={() => setRegisterOpen(false)} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleRegister} color="primary">
+            Register
+          </Button>
+        </DialogActions>
+      </Dialog>
+      </span>
+
+const logout =
+<Button style={{marginLeft:'100px'}} variant="text" color="inherit" onClick={handleLogout}>
+Logout
+</Button>
+
+const platform = user ? <h5> {logout} </h5> :  <h5> {login} | {register} </h5>
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div className="App">
+{platform}
+    <HashRouter>
+   <SideNav
+        className="sideNav"
+      >
+        <SideNav.Toggle />
+        <SideNav.Nav defaultSelected="home">
+          <NavItem eventKey="home">
+            <NavIcon>
+              <i className="fa fa-fw fa-home" style={{ fontSize: '1.75em' }} />
+            </NavIcon>
+            <NavText>
+              <NavLink to="/">Home</NavLink>
+            </NavText>
+          </NavItem>
+          <NavItem eventKey="1">
+            <NavIcon>
+              <i className="fa fa-fw fa-line-chart" style={{ fontSize: '1.75em' }} />
+            </NavIcon>
+            <NavText>
+               <NavLink to="/Songs">Songs</NavLink>
+            </NavText>
+          </NavItem>
+          <NavItem eventKey="2">
+            <NavIcon>
+              <i className="fa fa-fw fa-line-chart" style={{ fontSize: '1.75em' }} />
+            </NavIcon>
+            <NavText>
+           <NavLink to="/Artists">Artists</NavLink>
+            </NavText>
+          </NavItem>
+          <NavItem eventKey="3">
+            <NavIcon>
+              <i className="fa fa-fw fa-line-chart" style={{ fontSize: '1.75em' }} />
+            </NavIcon>
+            <NavText>
+               <NavLink to="/Albums">Albums</NavLink>
+            </NavText>
+          </NavItem>
+          <NavItem eventKey="4">
+            <NavIcon>
+              <i className="fa fa-fw fa-line-chart" style={{ fontSize: '1.75em' }} />
+            </NavIcon>
+            <NavText>
+             <NavLink to="/Playlists">Playlists</NavLink>
+            </NavText>
+          </NavItem>
+        </SideNav.Nav>
+      </SideNav>
+  <Route exact path="/" component={() => <Home user={user}/>}/>
+<Route path="/Songs" component={() => <Songs user={user}/>}/>
+<Route path="/Artists" component={() => <Artists user={user}/>}/>
+<Route path="/Playlists" component={() => <Playlists user={user}/>}/>
+<Route path="/Albums" component={() => <Albums user={user}/>}/>
+      </HashRouter>
     </div>
+
+    
   );
 }
 
---
+++ client/src/components/Playlists.js
@@ -0,0 +1,43 @@
+  
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function Playlists(props) {
+const [list, setList] = useState([])
+const [search, setSearch] = useState('')
+
+useEffect(() => {
+    const fetchData = async () => {
+      const playlists = await (await axios.get(`/top_playlists`)).data;
+      let list = playlists.filter(e => e.name.toUpperCase().includes(search.toUpperCase()))
+      makePlaylists(list)
+    }; fetchData();
+   }, [search])
+
+const makePlaylists = (playlists) => {
+let array = playlists.map(e => {
+return (
+<li key={e.id} class='grid-item'>
+<p>{e.name}</p>
+<img alt={e.name} width="150" height="150" src={e.cover_img}>
+</img>
+</li>
+)}
+)
+setList(array)
+}
+
+
+
+  return (
+<div> 
+<input className="filterList" placeholder="Search..." onChange={(event) => setSearch(event.target.value)} />
+<button className="Add">+</button>  
+<ul className="grid-container">
+{list}
+</ul>
+</div>
+  );
+}
+
+export default Playlists;
