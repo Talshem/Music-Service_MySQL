b7ac2dbe7ae6293f0fc84f86e73db736a0c47210
Talshem <talsh8797@gmail.com>
2020-09-12 02:23:33 +0300

    Update index.js

--- index.js
+++ index.js
@@ -26,10 +26,9 @@ con.connect(function(err) {
 
 app.use(express.static(__dirname + '/client/build'));
 
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
+app.get('*', (req, res) => {
+res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 ///////////////////////////////////////////////////////////////// USER
 
